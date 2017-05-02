365867369f47acc97decc622585d4689da44d00b
戴　岩沢 <daiyanze@EverySense-Japan-no-MacBook-Air.local>
2017-05-02 16:49:18 +0900

    rename doc to docs

---
+++ docs/assets/js/config.js
@@ -0,0 +1,23 @@
+var langs = [
+  {title: '日本語', path:'/', matchPath: /^\/(home)/},
+  {title: 'English', path: '/lang/en/', matchPath: /^\/en/}
+];
+
+docute.init({
+  repo: 'every-sense/UserDocument',
+  tocVisibleDepth: 4,
+  debug: true,
+  sidebar: true,
+  nav: {
+    default: [
+      {title: 'API説明書', path: '/'},
+      {title: '変更履歴', path: '/changelog'},
+      {title: '日本語', type: 'dropdown', items: langs}
+    ],
+    'lang/en': [
+      {title: 'API Reference', path: '/lang/en/'},
+      {title: 'ChangeLog', path: '/changelog'},
+      {title: 'English', type: 'dropdown', items: langs}
+    ]
+  }
+})
