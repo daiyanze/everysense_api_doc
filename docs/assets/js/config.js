57e4162222d83c86a313384f52bedc5b8f199431
戴　岩沢 <daiyanze@EverySense-Japan-no-MacBook-Air.local>
2017-05-08 15:53:40 +0900

    transfer master to dev \n revive folder lang

--- docs/assets/js/config.js
+++ docs/assets/js/config.js
@@ -1,3 +1,27 @@
+var langs = [
+  {title: '日本語', path:'/', matchPath: /^\/(home)/},
+  {title: 'English', path: '/lang/en/', matchPath: /^\/en/}
+];
+
+var nav = {
+  default: [
+    {title: 'API説明書', path: '/'},
+    {title: '変更履歴', path: '/changelog'},
+    {title: '日本語', type: 'dropdown', items: langs}
+  ],
+  'lang/en': [
+    {title: 'API Reference', path: '/lang/en/'},
+    {title: 'ChangeLog', path: '/changelog'},
+    {title: 'English', type: 'dropdown', items: langs}
+  ]
+}
+
+// var nav = {
+//   default: [
+//     {title: 'API説明書', path: '/'},
+//     {title: '変更履歴', path: '/changelog'}
+//   ]
+// }
 
 docute.init({
   repo: 'every-sense/UserDocument',
@@ -5,10 +29,5 @@ docute.init({
   debug: true,
   sidebar: true,
   'edit-link': 'https://github.com/daiyanze/everysense_api_doc/tree/master/docs/',
-  nav: {
-    default: [
-      {title: 'API説明書', path: '/'},
-      {title: 'ChangeLog', path: '/changelog'}
-    ]
-  }
+  nav: nav
 })
